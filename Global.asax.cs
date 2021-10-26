56430bd6d6c7c812fb5be9121f4026f148e67a4c
Akshaya2607 <80945908+Akshaya2607@users.noreply.github.com>
2021-10-26 09:45:27 +0530

    Master Page Demo

---
+++ Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace MasterPagesDemo
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["cnt"] = 0;
+        }
+        void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // HttpContext.Current.Session["v1"] = 100; //initialize v1 variable
+            Application["cnt"] = Convert.ToInt32(Application["cnt"]) + 1;
+        }
+        void Session_End(object sender, EventArgs e)
+        {
+
+            //Response.Write(Session["username"].ToString());
+            Session.Clear(); //or Session.RemoveAll();
+            //Session.Remove("username"); //if only 1 var
+            Session.Abandon();
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterPagesDemo
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = Session.SessionID.ToString();
+            Label2.Text = Session.IsNewSession.ToString();
+            Label3.Text = Session.IsCookieless.ToString();
+            Label4.Text = Application["cnt"].ToString();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtpwd.Text == "abc@123")
+            {
+                Session["username"] = txtuser.Text;
+                Response.Redirect("~/Statement.aspx");
+            }
+            else
+            {
+                Response.Write("Check username and password");
+            }
+        }
+    }
+}
