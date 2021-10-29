24e1705a99a4cf271164a7e7aaf241150a3980a2
Akshaya2607 <80945908+Akshaya2607@users.noreply.github.com>
2021-10-29 18:11:04 +0530

    Flight MVC Demo

---
+++ CrewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAppDemo.Models
+{
+    public class CrewModel
+    {
+        public int crewID { get; set; }
+        public string crewName { get; set; }
+        public string crewDesg { get; set; }
+
+    }
+}
---
+++ flCrModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAppDemo.Models
+{
+    public class flCrModel
+    {
+        public int flightID { get; set; }
+        public List<CrewModel> CrewMembers { get; set; }
+    }
+}
